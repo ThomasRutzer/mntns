fa26aad3fe9560da1f205dbff05a12265fad4905
Thomas Rutzer <thomas.rutzer@diepartments.de>
2018-02-13 20:54:54 +0100

    added mnts route

--- src/components/router/Router.ts
+++ src/components/router/Router.ts
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import VueRouter, { Location, Route, RouteConfig } from 'vue-router';
 
 const homeComponent = () => import('./../home').then(({ HomeComponent }) => HomeComponent);
+const experimentsListingComponent = () => import('./../experiments-listing').then(({ ExperimentsListingComponent }) => ExperimentsListingComponent);
+const mntsComponent = () => import('./../mnts').then(({ MntsComponent }) => MntsComponent);
 
 Vue.use(VueRouter);
 
@@ -10,7 +12,15 @@ export const createRoutes: () => RouteConfig[] = () => [
         path: '/',
         components: {
             content: homeComponent,
-            background: null
+            background: mntsComponent
+        },
+    },
+
+    {
+        path: '/mnts',
+        components: {
+            content: experimentsListingComponent,
+            background: mntsComponent
         },
     },
 ];
