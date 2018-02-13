08673c1d174699aa50cda7525d8f6efd8ce51cd2
Thomas Rutzer <thomas.rutzer@diepartments.de>
2018-02-13 20:55:33 +0100

    updated backgrund state prop

--- src/store/state.ts
+++ src/store/state.ts
@@ -1,5 +1,13 @@
 const state = {
-    backgroundExpanded: true
+    /**
+     * defines visibility of component, which Router
+     * renders into router-view=background
+     *
+     * @property {number}
+     * @value 0 background component is visibile in reduced size
+     * @value 1 background is visible and expanded
+     */
+    backgroundVisibility: 1
 };
 
 export default state;
