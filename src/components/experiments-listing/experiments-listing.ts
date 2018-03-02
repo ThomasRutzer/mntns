605712949abbeb21c07b95402dcb89edc166a983
Thomas Rutzer <thomas.rutzer@diepartments.de>
2018-03-02 18:09:37 +0100

    added focusedRepo data to template

--- src/components/experiments-listing/experiments-listing.ts
+++ src/components/experiments-listing/experiments-listing.ts
@@ -6,14 +6,34 @@ import { mutationTypes } from './../../store/';
     computed: {
         isActivated() {
             return this.$store.state.background.activated;
+        },
+
+        dataLoaded() {
+            return this.$store.state.currentRoute.titleAnimatedIn;
+        },
+
+        focusedData() {
+            return this.$store.state.gitHubData.focusedRepo.raw
+        },
+
+        focusedEvent() {
+            this.outside = this.$store.state.gitHubData.focusedRepo.event.mouseX > (window.innerWidth / 2);
+            return this.$store.state.gitHubData.focusedRepo.event
         }
     },
 })
 
 export class ExperimentsListingComponent extends Vue {
+    private outside: boolean = false;
 
     beforeCreate() {
         this.$store.commit(mutationTypes.EXPAND_BACKGROUND);
+        this.$store.commit(mutationTypes.DEACTIVATE_BACKGROUND);
+    }
+
+    beforeDestroy() {
+        this.$store.commit(mutationTypes.DEACTIVATE_BACKGROUND);
+        this.$store.commit(mutationTypes.UNFOCUS_REPO);
     }
 
     activate() {
