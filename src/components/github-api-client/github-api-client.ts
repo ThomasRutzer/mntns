96ea7c43bde5943b652c20fa3cf34ea64c2cee2b
Thomas Rutzer <thomas.rutzer@diepartments.de>
2018-03-06 16:09:40 +0100

    added method getCommits() to Github Api Client

--- src/components/github-api-client/github-api-client.ts
+++ src/components/github-api-client/github-api-client.ts
@@ -1,19 +1,37 @@
 import { injectable } from "inversify";
 import "reflect-metadata";
-import axios from 'axios';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import { GithubApiClientInterface } from "./github-api-client-interface";
 
 const baseUrl = 'https://api.github.com';
 
 @injectable()
 class GithubApiClient implements GithubApiClientInterface {
+    private httpClient: AxiosInstance;
+
+    constructor() {
+        this.httpClient = axios.create();
+
+        /**
+         * commits on github don´t have a @property id
+         */
+        this.httpClient.interceptors.response.use(function(response: AxiosResponse) {
+            if(response.config.url.includes('commit')) {
+                response.data.map((dataSet) => {
+                    dataSet.id = dataSet.sha;
+                })
+            }
+
+            return response;
+        });
+    }
 
     getUserRepos(userName: string) {
-        return axios.get(`${baseUrl}/users/${userName}/repos`);
+        return this.httpClient.get(`${baseUrl}/users/${userName}/repos`);
     }
 
     getCommits(repoName: string, userName: string) {
-        return axios.get(`${baseUrl}/repos/${userName}/${repoName}/commits`);
+        return this.httpClient.get(`${baseUrl}/repos/${userName}/${repoName}/commits`);
     }
 }
 
