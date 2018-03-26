ddea6aecf1e262beeb64db129d494a71b60519f5
Thomas Rutzer <thomas.rutzer@diepartments.de>
2018-03-26 22:14:13 +0200

    new markup for home component

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -1,10 +1,11 @@
 import { Component, Vue } from 'vue-property-decorator';
 import { mutationTypes } from './../../store/';
+import {RouterDefaultComponentAbstract} from '../router';
 
 @Component({
     template: require('./home.html')
 })
-export class HomeComponent extends Vue {
+export class HomeComponent extends RouterDefaultComponentAbstract {
     beforeCreate() {
         this.$store.commit(mutationTypes.REDUCE_BACKGROUND);
     }
