0203d49a44de9c10424f7eb01265eac6453bbce4
Thomas Rutzer <thomas.rutzer@diepartments.de>
2018-04-09 23:16:52 +0200

    * v-if background backdrop only if its not expanded

--- src/components/background/index.ts
+++ src/components/background/index.ts
@@ -1,5 +1,10 @@
+import Vue from 'vue';
 import { BackgroundComponent } from './background';
+import { BackgroundBackdropComponent } from './backdrop/background-backdrop';
+
+Vue.component('background-backdrop', BackgroundBackdropComponent);
 
 export {
-    BackgroundComponent
+    BackgroundComponent,
+    BackgroundBackdropComponent
 };
