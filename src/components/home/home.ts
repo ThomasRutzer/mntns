c365ec11eb859e4f4552c006a2c76de44430239c
Thomas Rutzer <thomas.rutzer@diepartments.de>
2018-04-09 23:18:55 +0200

    * updated home title

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -1,12 +1,18 @@
-import { Component, Vue } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
 import { mutationTypes } from './../../store/';
 import {RouterDefaultComponentAbstract} from '../router';
 
 @Component({
-    template: require('./home.html')
+    template: require('./home.html'),
 })
 export class HomeComponent extends RouterDefaultComponentAbstract {
+
     beforeCreate() {
+        this.$store.commit(mutationTypes.CONTENT_NOT_CLICKABLE);
         this.$store.commit(mutationTypes.REDUCE_BACKGROUND);
+    }
+
+    destroyed() {
+        this.$store.commit(mutationTypes.CONTENT_CLICKABLE);
     }
 }
