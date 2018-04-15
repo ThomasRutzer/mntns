13fc67e7fa5410989a6aeed2df123c96779d379c
Thomas Rutzer <thomas.rutzer@diepartments.de>
2018-04-15 14:28:34 +0200

    added mocks for Github Api Data

---
+++ mocks/github-commit-mock.ts
@@ -0,0 +1,154 @@
+export default [
+    {
+        "sha": "875670a38c40556f3c115dbeef1c4fd88cb240f2",
+        "commit": {
+            "author": {
+                "name": "Thomas Rutzer",
+                "email": "thomas.rutzer@diepartments.de",
+                "date": "2018-03-01T14:24:53Z"
+            },
+            "committer": {
+                "name": "Thomas Rutzer",
+                "email": "thomas.rutzer@diepartments.de",
+                "date": "2018-03-01T14:24:53Z"
+            },
+            "message": "changed state.background to its own object",
+            "tree": {
+                "sha": "15513bdd7f919148dad2e6e8642756b22ff77928",
+                "url": "https://api.github.com/repos/ThomasRutzer/portfolio/git/trees/15513bdd7f919148dad2e6e8642756b22ff77928"
+            },
+            "url": "https://api.github.com/repos/ThomasRutzer/portfolio/git/commits/875670a38c40556f3c115dbeef1c4fd88cb240f2",
+            "comment_count": 0,
+            "verification": {
+                "verified": false,
+                "reason": "unsigned",
+                "signature": null,
+                "payload": null
+            }
+        },
+        "url": "https://api.github.com/repos/ThomasRutzer/portfolio/commits/875670a38c40556f3c115dbeef1c4fd88cb240f2",
+        "html_url": "https://github.com/ThomasRutzer/portfolio/commit/875670a38c40556f3c115dbeef1c4fd88cb240f2",
+        "comments_url": "https://api.github.com/repos/ThomasRutzer/portfolio/commits/875670a38c40556f3c115dbeef1c4fd88cb240f2/comments",
+        "author": {
+            "login": "dp-thomas",
+            "id": 26769134,
+            "avatar_url": "https://avatars2.githubusercontent.com/u/26769134?v=4",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/dp-thomas",
+            "html_url": "https://github.com/dp-thomas",
+            "followers_url": "https://api.github.com/users/dp-thomas/followers",
+            "following_url": "https://api.github.com/users/dp-thomas/following{/other_user}",
+            "gists_url": "https://api.github.com/users/dp-thomas/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/dp-thomas/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/dp-thomas/subscriptions",
+            "organizations_url": "https://api.github.com/users/dp-thomas/orgs",
+            "repos_url": "https://api.github.com/users/dp-thomas/repos",
+            "events_url": "https://api.github.com/users/dp-thomas/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/dp-thomas/received_events",
+            "type": "User",
+            "site_admin": false
+        },
+        "committer": {
+            "login": "dp-thomas",
+            "id": 26769134,
+            "avatar_url": "https://avatars2.githubusercontent.com/u/26769134?v=4",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/dp-thomas",
+            "html_url": "https://github.com/dp-thomas",
+            "followers_url": "https://api.github.com/users/dp-thomas/followers",
+            "following_url": "https://api.github.com/users/dp-thomas/following{/other_user}",
+            "gists_url": "https://api.github.com/users/dp-thomas/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/dp-thomas/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/dp-thomas/subscriptions",
+            "organizations_url": "https://api.github.com/users/dp-thomas/orgs",
+            "repos_url": "https://api.github.com/users/dp-thomas/repos",
+            "events_url": "https://api.github.com/users/dp-thomas/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/dp-thomas/received_events",
+            "type": "User",
+            "site_admin": false
+        },
+        "parents": [
+            {
+                "sha": "6863a61dfbfd5d7afbf7d3ce5c23c41c5edb0295",
+                "url": "https://api.github.com/repos/ThomasRutzer/portfolio/commits/6863a61dfbfd5d7afbf7d3ce5c23c41c5edb0295",
+                "html_url": "https://github.com/ThomasRutzer/portfolio/commit/6863a61dfbfd5d7afbf7d3ce5c23c41c5edb0295"
+            }
+        ]
+    },
+    {
+        "sha": "ae673a41d606dfb7c9db7b63b0c43860f21e3959",
+        "commit": {
+            "author": {
+                "name": "Thomas Rutzer",
+                "email": "thomas.rutzer@diepartments.de",
+                "date": "2018-02-17T20:46:11Z"
+            },
+            "committer": {
+                "name": "Thomas Rutzer",
+                "email": "thomas.rutzer@diepartments.de",
+                "date": "2018-02-17T20:46:11Z"
+            },
+            "message": "various ui items",
+            "tree": {
+                "sha": "8f4db500344133556732dfa70978cdbaed4039d0",
+                "url": "https://api.github.com/repos/ThomasRutzer/portfolio/git/trees/8f4db500344133556732dfa70978cdbaed4039d0"
+            },
+            "url": "https://api.github.com/repos/ThomasRutzer/portfolio/git/commits/ae673a41d606dfb7c9db7b63b0c43860f21e3959",
+            "comment_count": 0,
+            "verification": {
+                "verified": false,
+                "reason": "unsigned",
+                "signature": null,
+                "payload": null
+            }
+        },
+        "url": "https://api.github.com/repos/ThomasRutzer/portfolio/commits/ae673a41d606dfb7c9db7b63b0c43860f21e3959",
+        "html_url": "https://github.com/ThomasRutzer/portfolio/commit/ae673a41d606dfb7c9db7b63b0c43860f21e3959",
+        "comments_url": "https://api.github.com/repos/ThomasRutzer/portfolio/commits/ae673a41d606dfb7c9db7b63b0c43860f21e3959/comments",
+        "author": {
+            "login": "dp-thomas",
+            "id": 26769134,
+            "avatar_url": "https://avatars2.githubusercontent.com/u/26769134?v=4",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/dp-thomas",
+            "html_url": "https://github.com/dp-thomas",
+            "followers_url": "https://api.github.com/users/dp-thomas/followers",
+            "following_url": "https://api.github.com/users/dp-thomas/following{/other_user}",
+            "gists_url": "https://api.github.com/users/dp-thomas/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/dp-thomas/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/dp-thomas/subscriptions",
+            "organizations_url": "https://api.github.com/users/dp-thomas/orgs",
+            "repos_url": "https://api.github.com/users/dp-thomas/repos",
+            "events_url": "https://api.github.com/users/dp-thomas/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/dp-thomas/received_events",
+            "type": "User",
+            "site_admin": false
+        },
+        "committer": {
+            "login": "dp-thomas",
+            "id": 26769134,
+            "avatar_url": "https://avatars2.githubusercontent.com/u/26769134?v=4",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/dp-thomas",
+            "html_url": "https://github.com/dp-thomas",
+            "followers_url": "https://api.github.com/users/dp-thomas/followers",
+            "following_url": "https://api.github.com/users/dp-thomas/following{/other_user}",
+            "gists_url": "https://api.github.com/users/dp-thomas/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/dp-thomas/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/dp-thomas/subscriptions",
+            "organizations_url": "https://api.github.com/users/dp-thomas/orgs",
+            "repos_url": "https://api.github.com/users/dp-thomas/repos",
+            "events_url": "https://api.github.com/users/dp-thomas/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/dp-thomas/received_events",
+            "type": "User",
+            "site_admin": false
+        },
+        "parents": [
+            {
+                "sha": "0e1aa03cd633758b8f91837f3f459bd680466169",
+                "url": "https://api.github.com/repos/ThomasRutzer/portfolio/commits/0e1aa03cd633758b8f91837f3f459bd680466169",
+                "html_url": "https://github.com/ThomasRutzer/portfolio/commit/0e1aa03cd633758b8f91837f3f459bd680466169"
+            }
+        ]
+    }
+];
