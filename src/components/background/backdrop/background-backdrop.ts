70a92e1e5eae5a530da3f3b3f5a49c788f4879b1
Thomas Rutzer <thomas.rutzer@diepartments.de>
2018-04-17 10:29:35 +0200

    background-backdrop can now deactivate mousefollow

--- src/components/background/backdrop/background-backdrop.ts
+++ src/components/background/backdrop/background-backdrop.ts
@@ -1,14 +1,26 @@
 import {Component, Prop, Vue, Watch} from 'vue-property-decorator';
+import { types, diContainer } from './../../dependency-injection';
+import {BREAKPOINTS, MediaQueryServiceInterface} from '../../media-queries';
 
 @Component({
     template: require('./background-backdrop.html'),
 })
 export class BackgroundBackdropComponent extends Vue {
+    private mousemoveActivated: boolean;
+    private mediaQueryService: MediaQueryServiceInterface;
 
     @Prop()
     activated: boolean;
 
     @Prop()
     triggerLabel: string;
+
+    created() {
+        const breakpoints = diContainer.get(types.Breakpoints);
+        this.mediaQueryService = diContainer.get<MediaQueryServiceInterface>(types.MediaQueryService);
+        this.mediaQueryService.on(breakpoints['m'], (mqEvent: MediaQueryList) => {
+            this.mousemoveActivated = mqEvent.matches;
+        });
+    }
 }
 
