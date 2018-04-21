385e6dc9052dfa77e1f4f566b1f77200a857fd62
Thomas Rutzer <thomas.rutzer@diepartments.de>
2018-04-21 22:58:57 +0200

    conditionaly hide footer

--- src/components/footerbar/footerbar.spec.ts
+++ src/components/footerbar/footerbar.spec.ts
@@ -1,10 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Vuex from 'vuex';
+
 import Component from 'vue-class-component';
 import { expect } from 'chai';
+import {stub} from 'sinon';
 import { ComponentTest } from '../../util/component-test';
 import { FooterbarComponent } from './footerbar';
 import RouterLink from './../router/Router-Link';
+import {diContainer, types} from '../dependency-injection';
 
 const links = [
     new RouterLink('Kontakt', '/contact'),
@@ -21,28 +25,48 @@ class MockFooterbarComponent extends FooterbarComponent {
 }
 
 describe('Footer component', () => {
-    let directiveTest: ComponentTest;
+    let directiveTest: ComponentTest, store, state;
     let router: VueRouter;
 
-    beforeEach(() => {
+    before(() => {
         Vue.use(VueRouter);
         directiveTest = new ComponentTest('<div><footerbar></footerbar></div>', { 'footerbar': MockFooterbarComponent });
 
         router = new VueRouter();
+
+        state = {
+            currentRoute: {
+                footerVisible: true
+            },
+        };
+
+        // stub DI
+        stub(diContainer, 'get')
+            .withArgs(types.Store).returns(new Vuex.Store({
+            state,
+        }));
+
+        store = diContainer.get(types.Store);
+    });
+
+    after(() => {
+        // @ts-ignore
+        // Property 'restore' does not exist on type '<T>(serviceIdentifier: string | symbol | Newable<T> | Abstract<T>) => T'.
+        diContainer.get.restore();
     });
 
     it('creates a list item for each Link in links', async () => {
-        directiveTest.createComponent();
+        directiveTest.createComponent({ router, store });
 
-        await directiveTest.execute((vm) => { // ensure Vue has bootstrapped/run change detection
+        await directiveTest.execute((vm) => {
             expect(vm.$el.querySelectorAll('.footerbar__list li').length).to.equal(links.length);
         });
     });
 
     it('displays link name', async () => {
-        directiveTest.createComponent({ router: router });
+        directiveTest.createComponent({ router, store });
 
-        await directiveTest.execute((vm) => { // ensure Vue has bootstrapped/run change detection
+        await directiveTest.execute((vm) => {
             expect(vm.$el.querySelectorAll('.footerbar__list-item')[0].textContent).to.equal(links[0].name);
         });
     });
--- src/components/footerbar/footerbar.ts
+++ src/components/footerbar/footerbar.ts
@@ -3,7 +3,13 @@ import RouterLinks from './../router/Router-Link';
 
 @Component({
     template: require('./footerbar.html'),
+    computed: {
+        isVisible() {
+            return this.$store.state.currentRoute.footerVisible;
+        }
+    }
 })
+
 export class FooterbarComponent extends Vue {
     links: RouterLinks[] = [
         new RouterLinks('Kontakt', 'contact'),
