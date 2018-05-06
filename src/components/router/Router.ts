462c5f5dde782ad895b2f07b6c46e3a7d966ffc0
Thomas Rutzer <thomas.rutzer@diepartments.de>
2018-05-06 21:31:45 +0200

    new build

--- src/components/router/Router.ts
+++ src/components/router/Router.ts
@@ -37,7 +37,6 @@ export const createRoutes: () => RouteConfig[] = () => [
 export const createRouter = () => new VueRouter({
     mode: 'history',
     routes: createRoutes(),
-    base: '/portfolio/dist/',
     scrollBehavior (to, from, savedPosition) {
         return { x: 0, y: 0 };
     }
