0865d7a1f2eb2699a73b50a89bce8881f0090768
Thomas Rutzer <thomas.rutzer@diepartments.de>
2018-06-07 23:07:47 +0200

    removed unnecessary lines of code found in HomeComponent

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from 'vue-property-decorator';
 import { mutationTypes } from './../../store/';
-import {RouterDefaultComponentAbstract} from '../router';
+import { RouterDefaultComponentAbstract } from '../router';
 
 @Component({
     template: require('./home.html'),
@@ -11,30 +11,11 @@ import {RouterDefaultComponentAbstract} from '../router';
     }
 })
 export class HomeComponent extends RouterDefaultComponentAbstract {
-    // private isStarted: boolean = false;
-
     beforeCreate() {
-       // this.$store.commit(mutationTypes.CONTENT_NOT_CLICKABLE);
         this.$store.commit(mutationTypes.REDUCE_EXPERIMENT_CONTAINER);
-
-       // this.$store.commit(mutationTypes.DEACTIVATE_EXPERIMENT_CONTAINER);
-    }
-
-
-    beforeDestroy() {
-        // this.$store.commit(mutationTypes.DEACTIVATE_EXPERIMENT_CONTAINER);
-       // this.$store.commit(mutationTypes.UNFOCUS_REPO);
-    }
-
-    destroyed() {
-        // this.$store.commit(mutationTypes.CONTENT_CLICKABLE);
     }
 
     startExperiment() {
-        // this.isStarted = true;
-        // this.$store.commit(mutationTypes.ACTIVATE_EXPERIMENT_CONTAINER);
-        // this.$store.commit(mutationTypes.FOOTER_VISIBLE, false);
-       // this.$store.commit(mutationTypes.EXPAND_EXPERIMENT_CONTAINER);
         this.$router.push('/experiment');
     }
 }
