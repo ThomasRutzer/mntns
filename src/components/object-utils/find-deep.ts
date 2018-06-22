52e60774929d38445a8f361b287007b741dfd01a
Thomas Rutzer <thomas.rutzer@diepartments.de>
2018-06-22 11:33:11 +0200

    updated findDeep helper to return null when nothing matches

--- src/components/object-utils/find-deep.ts
+++ src/components/object-utils/find-deep.ts
@@ -2,7 +2,7 @@
  * searches for nested property in
  * object, based on argument pathArr
  */
-const findDeep = (nestedObj: object, pathArr: string[]) => {
+const findDeep = (nestedObj: object, pathArr: string[]): string|null => {
     return pathArr.reduce((obj, key) =>
         (obj && obj[key] !== 'undefined') ? obj[key] : null, nestedObj);
 };
